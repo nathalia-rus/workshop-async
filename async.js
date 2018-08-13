0963fe4d4b70ba97ab918593462308d0553086c6
Nathalia Rus <nathaliarus@gmail.com>
2018-08-13 14:38:01 +0200

    not sure if I invented some stuff there

--- async.js
+++ async.js
@@ -7,18 +7,24 @@ const randomNumber = () => {
 
 // 1. Make it wait for 1 sec. with `setTimeout` and log it on main function
 const timeoutRandomNumber = () => {
-
-}
+  setTimeout( main (), 1000)};
 
 // 2. Now wrap the timeout version to work with promises
 const promiseRandomNumber = () => {
-
-}
+  return new Promise((resolve, reject) => {
+    timeoutRandomNumber();
+    if (err) reject(err);
+    else resolve(data);
+  })
+};
+  
 
 // 3. Finally, code a final version with async await.
 const asyncRandomNumber = () => {
-
-}
+  const randomNumberAwait = await promiseRandomNumber();
+  await write("randomNumber");
+  console.log(randomNumberAwait);
+};
 
 const rangedRandomNumber = (base, min, max) => {
   return Math.floor((base * (max - min)) + min);
